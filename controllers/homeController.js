4e521dc0587fdbf69a80314309967ddb4744d094
joeaycramer <joeay@live.co.uk>
2015-08-06 08:24:40 +0100

    Menu & velocity
    - New menu bar and menu icon
    - Velocity installed (better animation than queries engine)
    - sidebar added with items and welcome bit

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,12 +1,14 @@
 if (Meteor.isClient) {
 
-  homeController = baseController.extend({
-    show: function() {
-      this.render('home', {to: 'content'});
-      this.render('header', {to: 'header'});
-      this.render('footer', {to: 'footer'});
-      console.log('mans at home');
-    }
-  });
+	homeController = baseController.extend({
+		show: function() {
+			this.render('home', {to: 'content'});
+			this.render('head', {to: 'head'});
+			this.render('side-menu', {to: 'side-menu'});
+			this.render('header', {to: 'header'});
+			this.render('footer', {to: 'footer'});
+			console.log('mans at home');
+		}
+	});
 
 }

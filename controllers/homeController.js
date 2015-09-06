9482d1223464bd1f2d5554f0626a87f50ee333ef
Craig Pullar <craigmichaelpullar@gmail.com>
2015-09-06 18:05:37 +0100

    Added tag area

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,12 +3,11 @@ if (Meteor.isClient) {
 	homeController = baseController.extend({
 		show: function() {
 			Session.set('page_title','home');
-			this.render('card', {to: 'content'});
+			this.render('home', {to: 'content'});
 			this.render('head', {to: 'head'});
 			this.render('sideMenu', {to: 'sideMenu'});
 			this.render('header', {to: 'header'});
 			this.render('footer', {to: 'footer'});
-			console.log('mans at home');
 		}
 	});
 
